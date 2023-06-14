f32bc5f19d8729205bef82f5c5d59d09243442d7
Erick Neri <erickneriantunes@hotmail.com>
2023-06-14 16:29:08 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -15,9 +15,15 @@ namespace tabuleiro
             Pecas = new Peca[linhas, Colunas];
         }
 
-        public Peca peca(int linha, int coluna)
+        public Peca Peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
         }
     }
 }
