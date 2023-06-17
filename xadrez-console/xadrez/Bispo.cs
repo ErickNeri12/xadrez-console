66a6c5962e6b0bb99b7ab5f04cf85e3db487ed63
Erick Neri <erickneriantunes@hotmail.com>
2023-06-17 17:24:52 -0300

    Acrescentando as demais peças

---
+++ xadrez-console/xadrez/Bispo.cs
@@ -0,0 +1,58 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    internal class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool _podeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //nordeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            
+            //sudeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            
+            //sudoeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            
+            //noroeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            return mat;
+        }
+    }
+}
