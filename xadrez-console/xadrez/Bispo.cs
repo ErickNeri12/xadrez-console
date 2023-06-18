0900cfb0723daddc185a63ce491c50d09a8e8d4d
Erick Neri <erickneriantunes@hotmail.com>
2023-06-17 22:15:24 -0300

    Jogada especial, roque pequeno e roque grande

--- xadrez-console/xadrez/Bispo.cs
+++ xadrez-console/xadrez/Bispo.cs
@@ -27,30 +27,53 @@ namespace xadrez
 
             //nordeste
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
-            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            while (Tab.PosicaoValida(pos) && _podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.Linha - 1, pos.Coluna + 1);
             }
             
+
             //sudeste
             pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
-            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            while (Tab.PosicaoValida(pos) && _podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.Linha + 1, pos.Coluna + 1);
             }
             
+
             //sudoeste
             pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
-            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            while (Tab.PosicaoValida(pos) && _podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.Linha + 1, pos.Coluna - 1);
             }
             
+
             //noroeste
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
-            if (Tab.PosicaoValida(pos) && _podeMover(pos))
+            while(Tab.PosicaoValida(pos) && _podeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
+                if(Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.Linha - 1, pos.Coluna - 1);
             }
             return mat;
         }
